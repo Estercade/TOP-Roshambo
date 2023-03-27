2e51ea830b03dccf8669388766e4f6bb3012724d
Dyeus <withrwithoutyou@gmail.com>
2023-03-27 12:59:03 -0400

    Add game function and score

--- js/javascript.js
+++ js/javascript.js
@@ -8,26 +8,22 @@ function getComputerChoice() {
     return hand[Math.floor(Math.random() * 3)];
 }
 
+let playerSelection;
 // Prompt user for their hand
 function getUserChoice() {
-    let playerSelection = prompt("What shape would you like to throw?")
-    playerSelection = playerSelection.toLowerCase();
+    return prompt("What shape would you like to throw?")
 }
 
 // Initialize result message
 let result;
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
 // Simulate a round of rock paper scissors
 // Compare user's hand versus the computer's
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === computerSelection) {
-        // If hands are the same
-        return result = "draw"
-        // Say it is a draw
+    // If hands are the same
+        return result = "It's a draw, try again!"
+        // Result is a draw
     // Else if the user's hand is rock
     } else if (playerSelection === 'rock') {
         // If the computer's hand is paper
@@ -37,6 +33,7 @@ function playRound(playerSelection, computerSelection) {
         // Else if the computer's hand is scissors
         } else if (computerSelection === 'scissors') {
             // Tell the user "You Win! Rock beats Scissors"
+            score += +1;
             return result = "You Win! Rock beats Scissors";}
     // Else if the user's hand is paper
     } else if (playerSelection === 'paper') {
@@ -47,6 +44,7 @@ function playRound(playerSelection, computerSelection) {
         // Else if the computer's hand is rock
         } else if (computerSelection === 'rock') {
             // Tell the user "You Win! Paper beats Rock"
+            score += +1;
             return result = "You Win! Paper beats Rock";}
     // Else if the user's hand is scissors
     } else if (playerSelection === 'scissors') {
@@ -57,6 +55,24 @@ function playRound(playerSelection, computerSelection) {
         // Else if the computer's hand is paper
         } else if (computerSelection === 'paper') {
             // Tell the user "You Win! Scissors beats Paper"
+            score += +1;
             return result = "You Win! Scissors beats Paper";}
         }
+}
+
+// Initialize number of rounds
+let n = 5;
+// Initialize score counter
+let score = 0;
+
+function game() {
+    for (i = 0; i < n; i++) {
+        console.log(`Round ${i + 1}`)
+        playerSelection = getUserChoice();
+        // Convert player selection to lowercase
+        playerSelection = playerSelection.toLowerCase();
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    }
+    console.log(`You won ${score} out of 5 rounds!`)
 }
