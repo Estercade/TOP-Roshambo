fb4e1e07ea4abdc4f19c8e216dfbae83b0b65883
Estercade <withrwithoutyou@gmail.com>
2023-03-27 20:03:15 -0400

    Remove some comments for better clarity

--- js/javascript.js
+++ js/javascript.js
@@ -28,33 +28,24 @@ function playRound(playerSelection, computerSelection) {
     } else if (playerSelection === 'rock') {
         // If the computer's hand is paper
         if (computerSelection === 'paper') {
-            // Tell the user "You Lose! Paper beats Rock"
             return result = "You Lose! Paper beats Rock";
-        // Else if the computer's hand is scissors
         } else if (computerSelection === 'scissors') {
-            // Tell the user "You Win! Rock beats Scissors"
             score += +1;
             return result = "You Win! Rock beats Scissors";}
     // Else if the user's hand is paper
     } else if (playerSelection === 'paper') {
         // If the computer's hand is scissors
         if (computerSelection === 'scissors') {
-            // Tell the user "You Lose! Scissors beats Paper"
             return result = "You Lose! Scissors beats Paper";
-        // Else if the computer's hand is rock
         } else if (computerSelection === 'rock') {
-            // Tell the user "You Win! Paper beats Rock"
             score += +1;
             return result = "You Win! Paper beats Rock";}
     // Else if the user's hand is scissors
     } else if (playerSelection === 'scissors') {
         // If the computer's hand is rock
         if (computerSelection === 'rock') {
-            // Tell the user "You Lose! Rock beats Scissors"
             return result = "You Lose! Rock beats Scissors";
-        // Else if the computer's hand is paper
         } else if (computerSelection === 'paper') {
-            // Tell the user "You Win! Scissors beats Paper"
             score += +1;
             return result = "You Win! Scissors beats Paper";}
         }
