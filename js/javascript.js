13266616f58942598201931030487076fef42a92
Estercade <withrwithoutyou@gmail.com>
2023-03-31 21:12:35 -0400

    Add event listeners for play buttons
    Fix logic of playRound() to accept button inputs

--- js/javascript.js
+++ js/javascript.js
@@ -8,66 +8,93 @@ function getComputerChoice() {
     return hand[Math.floor(Math.random() * 3)];
 }
 
+// Initalize player hand
 let playerSelection;
-// Prompt user for their hand
-function getUserChoice() {
-    return prompt("What shape would you like to throw?")
-}
 
 // Initialize result message
 let result;
+// Initialize score counter
+let scoreCounter = 0;
+// Initalize round counter
+let roundCounter = 0;
+// Initalize scoreboard
+let gameResult = 0;
 
 // Simulate a round of rock paper scissors
 // Compare user's hand versus the computer's
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice();
     if (playerSelection === computerSelection) {
     // If hands are the same
         return result = "It's a draw, try again!"
         // Result is a draw
+    } else {
+        roundCounter++;
+    }
     // Else if the user's hand is rock
-    } else if (playerSelection === 'rock') {
-        // If the computer's hand is paper
+    if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
             return result = "You Lose! Paper beats Rock";
         } else if (computerSelection === 'scissors') {
-            score += +1;
+            scoreCounter++;
             return result = "You Win! Rock beats Scissors";}
     // Else if the user's hand is paper
     } else if (playerSelection === 'paper') {
-        // If the computer's hand is scissors
         if (computerSelection === 'scissors') {
             return result = "You Lose! Scissors beats Paper";
         } else if (computerSelection === 'rock') {
-            score += +1;
+            scoreCounter++;
             return result = "You Win! Paper beats Rock";}
     // Else if the user's hand is scissors
     } else if (playerSelection === 'scissors') {
-        // If the computer's hand is rock
         if (computerSelection === 'rock') {
             return result = "You Lose! Rock beats Scissors";
         } else if (computerSelection === 'paper') {
-            score += +1;
+            scoreCounter++;
             return result = "You Win! Scissors beats Paper";}
         }
 }
 
-// Initialize number of rounds
-let n = 5;
-// Initialize score counter
-let score = 0;
+const playRock = document.getElementById('playRock');
+const playPaper = document.getElementById('playPaper');
+const playScissors = document.getElementById('playScissors');
 
-function game() {
-    for (i = 0; i < n; i++) {
-        console.log(`Round ${i + 1}`)
-        playerSelection = getUserChoice();
-        // Convert player selection to lowercase
-        playerSelection = playerSelection.toLowerCase();
-        let computerSelection = getComputerChoice();
-        playRound(playerSelection, computerSelection);
-        if (playerSelection === computerSelection) {
-            i--;
-        }
-        console.log(result);
+playRock.addEventListener('click', () => {
+    playerSelection = `rock`;
+    playRound(playerSelection);
+    updateResults();
+})
+
+playPaper.addEventListener('click', () => {
+    playerSelection = `paper`;
+    playRound(playerSelection);
+    updateResults();
+})
+
+playScissors.addEventListener('click', () => {
+    playerSelection = `scissors`;
+    playRound(playerSelection);
+    updateResults();
+})
+
+const roundResult = document.getElementById('roundResult');
+const scoreboard = document.getElementById('scoreboard')
+
+function updateResults() {
+    roundResult.textContent = result;
+    if (roundCounter < 5) {
+        scoreboard.textContent = `You have won ${scoreCounter} out of ${roundCounter} rounds!`;
+    } else if (roundCounter === 5 && scoreCounter < 3) {
+        scoreboard.textContent = `You have won ${scoreCounter} out of ${roundCounter} rounds! The computer has won!`;
+    } else if (roundCounter === 5 && scoreCounter >= 3) {
+        scoreboard.textContent = `You have won ${scoreCounter} out of ${roundCounter} rounds! You have won!`;
     }
-    console.log(`You won ${score} out of 5 rounds!`)
+    if (roundCounter >= 5) {
+        resetScore();
+    }
+}
+
+function resetScore() {
+    roundCounter = 0;
+    scoreCounter = 0;
 }
